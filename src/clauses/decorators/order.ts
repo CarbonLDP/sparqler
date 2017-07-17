e82f3071462ae7ffa9fc845b55045e0342d5e25e
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2017-07-17 12:46:43 -0500

    Add order doc comments

--- src/clauses/decorators/order.ts
+++ src/clauses/decorators/order.ts
@@ -16,7 +16,17 @@ import {
 	Token,
 } from "sparqler/tokens";
 
-
+/**
+ * Set a condition to be used as the order of the sequence of solutions the
+ * query will retrieve.
+ *
+ * With the current version of SPARQLER the conditions will not be evaluated
+ * for errors.
+ *
+ * @param rawCondition Raw condition that to be applied for the solutions order.
+ * @returns Object clause with the next possible methods than can be applied to
+ * the query.
+ */
 export function orderBy<T extends FinishClause | GraphPattern>( this:Container<T>, rawCondition:string ):LimitOffsetClause<T> & T {
 	const tokens:Token[] = [ ORDER, BY, new StringLiteral( rawCondition ) ];
 
@@ -24,6 +34,14 @@ export function orderBy<T extends FinishClause | GraphPattern>( this:Container<T
 	return this._finishDecorator<LimitOffsetClause<T>>( container, limitOffsetDecorator<T, {}>( container, {} ) );
 }
 
+/**
+ * Decorator that bind the OrderClause methods to a container and adds them
+ * to the provided object.
+ *
+ * @param container The container to bind the methods
+ * @param object Object to be decorated with the bound methods.
+ * @returns The same object provided that has been decorated.
+ */
 export function orderDecorator<T extends FinishClause | GraphPattern, W extends object>( container:Container<T>, object:W ):W & OrderClause<T> {
 	return genericDecorator( { orderBy }, container, limitOffsetDecorator<T, W>( container, object ) );
 }
