b081e3af382cf3b5ab4cfde060824101a633e9c8
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2017-07-19 14:00:24 -0500

    Add `query` module doc comments

--- dist/clauses/decorators/query.d.ts
+++ dist/clauses/decorators/query.d.ts
@@ -1,3 +1,3 @@
-import { FinishClause, QueryClause } from "sparqler/clauses";
 import { Container } from "sparqler/clauses/Container";
+import { FinishClause, QueryClause } from "sparqler/clauses/interfaces";
 export declare function queryDecorator<T extends FinishClause, W extends object>(container: Container<T>, object: W): W & QueryClause<T>;
--- src/clauses/decorators/query.ts
+++ src/clauses/decorators/query.ts
@@ -1,9 +1,9 @@
+import { Container } from "sparqler/clauses/Container";
+import { selectDecorator } from "sparqler/clauses/decorators";
 import {
 	FinishClause,
 	QueryClause,
-} from "sparqler/clauses";
-import { Container } from "sparqler/clauses/Container";
-import { selectDecorator } from "sparqler/clauses/decorators";
+} from "sparqler/clauses/interfaces";
 import { genericDecorator } from "sparqler/clauses/utils";
 import { IRIResolver } from "sparqler/iri/IRIResolver";
 import {
@@ -18,6 +18,15 @@ import {
 	Token,
 } from "sparqler/tokens";
 
+/**
+ * Add a base IRI the query uses to resolve any relative IRIs.
+ *
+ * If a default vocabulary is set with the {@link QueryClause.vocab}
+ * method, the base is ignored for relative predicates.
+ *
+ * @param iri IRI to be used as the query BASE.
+ * @returns Object with the methods to keep constructing the query.
+ */
 function base<T extends FinishClause>( this:Container<T>, iri:string ):QueryClause<T> {
 	const tokens:Token[] = [ BASE, OPEN_IRI, new StringLiteral( iri ), CLOSE_IRI ];
 
@@ -25,6 +34,13 @@ function base<T extends FinishClause>( this:Container<T>, iri:string ):QueryClau
 	return queryDecorator<T, {}>( container, {} );
 }
 
+/**
+ * Add a default vocabulary to be used to resolve relative IRIs when
+ * used as a predicate in a triple pattern.
+ *
+ * @param iri IRI to append to prepend to any relative predicate.
+ * @returns Object with the methods to keep constructing the query.
+ */
 function vocab<T extends FinishClause>( this:Container<T>, iri:string ):QueryClause<T> {
 	const iriResolver:IRIResolver = new IRIResolver( this._iriResolver, iri );
 
@@ -32,6 +48,16 @@ function vocab<T extends FinishClause>( this:Container<T>, iri:string ):QueryCla
 	return queryDecorator<T, {}>( container, {} );
 }
 
+/**
+ * Add a prefix to the query.
+ *
+ * If the prefix is not used in any part of the query it is not added
+ * in the compact query string.
+ *
+ * @param name Name the prefix will be identified for.
+ * @param iri The IRI of the current PREFIX.
+ * @returns Object with the methods to keep constructing the query.
+ */
 function prefix<T extends FinishClause>( this:Container<T>, name:string, iri:string ):QueryClause<T> {
 	const iriResolver:IRIResolver = new IRIResolver( this._iriResolver );
 	iriResolver._prefixes.set( name, false );
@@ -42,6 +68,14 @@ function prefix<T extends FinishClause>( this:Container<T>, name:string, iri:str
 	return queryDecorator<T, {}>( container, {} );
 }
 
+/**
+ * Decorator that bind {@link QueryClause} methods to container and
+ * adds them to the provided object.
+ *
+ * @param container The container where to bind the respective methods.
+ * @param object Object to be decorated with the bound methods.
+ * @returns The same object provided that has been decorated.
+ */
 export function queryDecorator<T extends FinishClause, W extends object>( container:Container<T>, object:W ):W & QueryClause<T> {
 	return genericDecorator( { base, vocab, prefix }, container, selectDecorator<T, W>( container, object ) );
 }
