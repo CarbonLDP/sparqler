e978e3a1fdbf115f332d9a2f11da9e5492cd8f1b
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2017-08-02 12:01:44 -0500

    Fix not showing <T> generic and add links to MDN web docs

--- build/docs/dgeni/rendering/filters/linkify.ts
+++ build/docs/dgeni/rendering/filters/linkify.ts
@@ -1,7 +1,80 @@
 import { Document } from "dgeni";
 import { Filter } from "./Filter";
 
-const IDENTIFIERS_REGEX:RegExp = /((?::|=>?|&lt;|&amp;|\||extends|,|^) ?)([_$a-zA-Z][_$a-zA-Z0-9]*)/g;
+const IDENTIFIERS_REGEX:RegExp = /((?::|=>?|<|&lt;|&|&amp;|\||extends|,|^) ?)([_$a-zA-Z][_$a-zA-Z0-9]*)/g;
+
+const MDN_URL:string = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/";
+// noinspection SpellCheckingInspection
+const MDN_IDENTIFIERS:string[] = [
+	"Array",
+	"ArrayBuffer",
+	"AsyncFunction",
+	"Atomics",
+	"Boolean",
+	"DataView",
+	"Date",
+	"Error",
+	"EvalError",
+	"Float32Array",
+	"Float64Array",
+	"Function",
+	"Generator",
+	"GeneratorFunction",
+	"Infinity",
+	"Int16Array",
+	"Int32Array",
+	"Int8Array",
+	"InternalError",
+	"Intl",
+	"Collator",
+	"DateTimeFormat",
+	"NumberFormat",
+	"Iterator",
+	"JSON",
+	"Map",
+	"Math",
+	"NaN",
+	"Number",
+	"Object",
+	"ParallelArray",
+	"Promise",
+	"Proxy",
+	"RangeError",
+	"ReferenceError",
+	"Reflect",
+	"RegExp",
+	"SIMD",
+	"Bool16x8",
+	"Bool32x4",
+	"Bool64x2",
+	"Bool8x16",
+	"Float32x4",
+	"Float64x2",
+	"Int16x8",
+	"Int32x4",
+	"Int8x16",
+	"Uint16x8",
+	"Uint32x4",
+	"Uint8x16",
+	"Set",
+	"SharedArrayBuffer",
+	"StopIteration",
+	"String",
+	"Symbol",
+	"SyntaxError",
+	"TypeError",
+	"TypedArray",
+	"URIError",
+	"Uint16Array",
+	"Uint32Array",
+	"Uint8Array",
+	"Uint8ClampedArray",
+	"WeakMap",
+	"WeakSet",
+	"WebAssembly",
+	"null",
+	"undefined",
+];
 
 export function linkify( getLinkInfo ):Filter {
 	return {
@@ -19,9 +92,14 @@ export function linkify( getLinkInfo ):Filter {
 				.replace( IDENTIFIERS_REGEX, ( match:string, before:string, identifier:string ) => {
 					const linkInfo = getLinkInfo( identifier, identifier, doc );
 
-					if( ! linkInfo.valid ) return match;
+					if( ! linkInfo.valid ) {
+						if( ! MDN_IDENTIFIERS.includes( identifier ) ) return match;
 
-					return `${ before }<a href="${ linkInfo.url }">${ identifier }</a>`;
+						linkInfo.url = MDN_URL + identifier;
+						linkInfo.external = true;
+					}
+
+					return `${ before }<a href="${ linkInfo.url }"${ linkInfo.external ? ` target="_blank"` : "" }>${ identifier }</a>`;
 				} )
 				;
 		},
