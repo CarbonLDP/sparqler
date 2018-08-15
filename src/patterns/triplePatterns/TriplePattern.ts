4310a4ee9ebda0ddff9760898cdcca8c518302fa
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-08-15 12:20:17 -0500

    Add missing docs for TriplePattern constant

--- src/patterns/triplePatterns/TriplePattern.ts
+++ src/patterns/triplePatterns/TriplePattern.ts
@@ -12,7 +12,22 @@ import { Pattern } from "../Pattern";
 export interface TriplePattern<T extends ObjectToken = ObjectToken> extends Pattern<TripleToken<T>> {}
 
 
+/**
+ * Constant with utils for {@link TriplePattern} objects.
+ */
 export const TriplePattern:{
+	/**
+	 * Factory function that allows to crete a {@link TriplePattern}
+	 * from the {@param object} provided.
+	 *
+	 * @param container The related container with the data for the
+	 * {@link TriplePattern} statement.
+	 * @param object The base base from where to create the
+	 * {@link TriplePattern} statement.
+	 *
+	 * @return The {@link TriplePattern} statement created from the
+	 * {@param object} provided.
+	 */
 	createFrom<T extends ObjectToken, C extends Container<TripleToken<T>>, O extends object>( container:C, object:O ):O & TriplePattern<T>;
 } = {
 	createFrom: Pattern.createFrom,
