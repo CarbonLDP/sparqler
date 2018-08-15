87592d8591a13e2b82613986c95a0f823ef6b06c
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-08-15 14:20:03 -0500

    Add test and docs for Pattern

--- src/patterns/Pattern.ts
+++ src/patterns/Pattern.ts
@@ -4,7 +4,7 @@ import { PatternToken } from "../tokens/PatternToken";
 
 
 /**
- * @todo
+ * Object that contains any type of pattern.
  */
 export interface Pattern<T extends PatternToken = PatternToken> {
 	getPattern():T;
@@ -12,9 +12,23 @@ export interface Pattern<T extends PatternToken = PatternToken> {
 
 
 /**
- * @todo
+ * Constant with utils for {@link Pattern} objects.
  */
-export const Pattern = {
+export const Pattern:{
+	/**
+	 * Factory function that allows to crete a {@link Pattern}
+	 * from the {@param object} provided.
+	 *
+	 * @param container The related container with the data for the
+	 * {@link Pattern} statement.
+	 * @param object The base base from where to create the
+	 * {@link Pattern} statement.
+	 *
+	 * @return The {@link Pattern} statement created from the
+	 * {@param object} provided.
+	 */
+	createFrom<T extends PatternToken, C extends Container<T>, O extends object>( container:C, object:O ):O & Pattern<T>;
+} = {
 	createFrom<T extends PatternToken, C extends Container<T>, O extends object>( container:C, object:O ):O & Pattern<T> {
 		return Object.assign( object, {
 			getPattern: () => container.targetToken,
