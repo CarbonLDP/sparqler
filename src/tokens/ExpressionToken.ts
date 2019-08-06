7c665e05988a0c59515474859ffa8ff93464b7c1
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2019-08-06 13:36:14 -0500

    Add documentation for ExpressionToken and PrimaryExpressionToken

--- src/tokens/ExpressionToken.ts
+++ src/tokens/ExpressionToken.ts
@@ -1,3 +1,9 @@
 import { PrimaryExpressionToken } from "./PrimaryExpressionToken";
 
+
+/**
+ * Alias with the tokens that comprehends all the kinds of expressions.
+ *
+ * @see {@link https://www.w3.org/TR/sparql11-query/#rExpression}
+ */
 export type ExpressionToken = PrimaryExpressionToken;
--- src/tokens/PrimaryExpressionToken.ts
+++ src/tokens/PrimaryExpressionToken.ts
@@ -4,5 +4,11 @@ import { LiteralToken } from "./LiteralToken";
 import { RDFLiteralToken } from "./RDFLiteralToken";
 import { VariableToken } from "./VariableToken";
 
+
 // TODO: Add `BracketedExpression`
+/**
+ * Alias with the tokens that comprehends all the kinds of primary expressions.
+ *
+ * @see {@link https://www.w3.org/TR/sparql11-query/#rPrimaryExpression}
+ */
 export type PrimaryExpressionToken = FunctionToken | VariableToken | IRIToken | RDFLiteralToken | LiteralToken;
