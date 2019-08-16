9355b4820ce5b966c0076de052b011309fb7f508
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2019-08-16 14:30:57 -0500

    Add factory for ExpressionsBuilder

--- src/patterns/expressions/ExpressionsBuilder.ts
+++ src/patterns/expressions/ExpressionsBuilder.ts
@@ -1,4 +1,39 @@
+import { Container } from "../../data/Container";
+import { Factory } from "../../data/Factory";
 import { FunctionExpressionsBuilder } from "./FunctionExpressionsBuilder";
+import { OperationExpressionsBuilder } from "./OperationExpressionsBuilder";
 
-interface ExpressionsBuilder extends FunctionExpressionsBuilder {
+
+/**
+ * Builder with the methods that helps you to expressions.
+ *
+ * See https://www.w3.org/TR/sparql11-query/#expressions for
+ * more information.
+ */
+interface ExpressionsBuilder extends FunctionExpressionsBuilder, OperationExpressionsBuilder {
 }
+
+
+/**
+ * Constant with the utils for {@link ExpressionsBuilder} objects.
+ */
+export const ExpressionsBuilder:{
+	/**
+	 * Factory function that allows to crete a {@link ExpressionsBuilder}
+	 * from the {@param object} provided.
+	 *
+	 * @param container The related container with the data for the
+	 * {@link ExpressionsBuilder} statement.
+	 * @param object The base base from where to create the
+	 * {@link ExpressionsBuilder} statement.
+	 *
+	 * @return The {@link ExpressionsBuilder} statement created from the
+	 * {@param object} provided.
+	 */
+	createFrom<O extends object>( container:Container<undefined>, object:O ):O & ExpressionsBuilder;
+} = {
+	createFrom: Factory.createFrom(
+		FunctionExpressionsBuilder.createFrom,
+		OperationExpressionsBuilder.createFrom,
+	),
+};
