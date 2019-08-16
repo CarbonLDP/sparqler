1d742a306688716b5ede701e1b418cd6bc3233a3
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2019-08-16 14:25:23 -0500

    Add comment docs

--- src/patterns/expressions/OperationExpressionsBuilder.ts
+++ src/patterns/expressions/OperationExpressionsBuilder.ts
@@ -14,30 +14,200 @@ import { Expression } from "./Expression";
 import { getBinaryOperationFn, getInclusionFn, getUnaryOperationFn } from "./fns/operationFn";
 
 
+/**
+ * Builder for operation expressions.
+ */
 export interface OperationExpressionsBuilder {
-	or( expression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
-	and( expression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns logical ORs
+	 * between the {@param leftExpression} and every {@param restExpressions}.
+	 *
+	 * See https://www.w3.org/TR/sparql11-query/#func-logical-or
+	 * for more information.
+	 *
+	 * @param leftExpression - Left expression for the logical OR
+	 * @param restExpressions - Expressions to construct a logical OR with the previous expression.
+	 */
+	or( leftExpression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns logical ANDs
+	 * between the {@param leftExpression} and every {@param restExpressions}.
+	 *
+	 * See https://www.w3.org/TR/sparql11-query/#func-logical-and
+	 * for more information.
+	 *
+	 * @param leftExpression - Left expression for the logical AND
+	 * @param restExpressions - Expressions to construct a logical AND with the previous expression.
+	 */
+	and( leftExpression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
 
-	equals( leftExpressions:Expression | SupportedNativeTypes, rightExpressions:Expression | SupportedNativeTypes ):Expression;
-	notEquals( leftExpressions:Expression | SupportedNativeTypes, rightExpressions:Expression | SupportedNativeTypes ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns `true` if
+	 * {@param leftExpression} and {@param rightExpression} are the same RDF term.
+	 *
+	 * See https://www.w3.org/TR/sparql11-query/#func-RDFterm-equal
+	 * for more information.
+	 *
+	 * @param leftExpression - Left expression to be compared against the {@param rightExpression}
+	 * @param rightExpression - Right expression to be compared against the {@param leftExpression}.
+	 */
+	equals( leftExpression:Expression | SupportedNativeTypes, rightExpression:Expression | SupportedNativeTypes ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns `true` if
+	 * {@param leftExpression} and {@param rightExpression} are NOT the same RDF term.
+	 *
+	 * See https://www.w3.org/TR/sparql11-query/#func-RDFterm-equal
+	 * for more information.
+	 *
+	 * @param leftExpression - Left expression to be compared against the {@param rightExpression}
+	 * @param rightExpression - Right expression to be compared against the {@param leftExpression}.
+	 */
+	notEquals( leftExpression:Expression | SupportedNativeTypes, rightExpression:Expression | SupportedNativeTypes ):Expression;
 
-	lt( leftExpressions:Expression | SupportedNativeTypes, rightExpressions:Expression | SupportedNativeTypes ):Expression;
-	lte( leftExpressions:Expression | SupportedNativeTypes, rightExpressions:Expression | SupportedNativeTypes ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns `true` if
+	 * {@param leftExpression} is less than the {@param rightExpression}.
+	 *
+	 * See the table *SPARQL Binary Operators* on
+	 * https://www.w3.org/TR/sparql11-query/#OperatorMapping for more information.
+	 *
+	 * @param leftExpression - Left expression to be compared against the {@param rightExpression}
+	 * @param rightExpression - Right expression to be compared against the {@param leftExpression}.
+	 */
+	lt( leftExpression:Expression | SupportedNativeTypes, rightExpression:Expression | SupportedNativeTypes ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns `true` if
+	 * {@param leftExpression} is less or equal than the {@param rightExpression}.
+	 *
+	 * See the table *SPARQL Binary Operators* on
+	 * https://www.w3.org/TR/sparql11-query/#OperatorMapping for more information.
+	 *
+	 * @param leftExpression - Left expression to be compared against the {@param rightExpression}
+	 * @param rightExpression - Right expression to be compared against the {@param leftExpression}.
+	 */
+	lte( leftExpression:Expression | SupportedNativeTypes, rightExpression:Expression | SupportedNativeTypes ):Expression;
 
-	gt( leftExpressions:Expression | SupportedNativeTypes, rightExpressions:Expression | SupportedNativeTypes ):Expression;
-	gte( leftExpressions:Expression | SupportedNativeTypes, rightExpressions:Expression | SupportedNativeTypes ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns `true` if
+	 * {@param leftExpression} is greater than the {@param rightExpression}.
+	 *
+	 * See the table *SPARQL Binary Operators* on
+	 * https://www.w3.org/TR/sparql11-query/#OperatorMapping for more information.
+	 *
+	 * @param leftExpression - Left expression to be compared against the {@param rightExpression}
+	 * @param rightExpression - Right expression to be compared against the {@param leftExpression}.
+	 */
+	gt( leftExpression:Expression | SupportedNativeTypes, rightExpression:Expression | SupportedNativeTypes ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns `true` if
+	 * {@param leftExpression} is greater or equal than the {@param rightExpression}.
+	 *
+	 * See the table *SPARQL Binary Operators* on
+	 * https://www.w3.org/TR/sparql11-query/#OperatorMapping for more information.
+	 *
+	 * @param leftExpression - Left expression to be compared against the {@param rightExpression}
+	 * @param rightExpression - Right expression to be compared against the {@param leftExpression}.
+	 */
+	gte( leftExpression:Expression | SupportedNativeTypes, rightExpression:Expression | SupportedNativeTypes ):Expression;
 
-	in( expression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
-	notIn( expression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns true whether
+	 * the {@param leftExpression} is equal to any of the {@param rightExpressions} values.
+	 *
+	 * See https://www.w3.org/TR/sparql11-query/#func-in
+	 * for more information.
+	 *
+	 * @param leftExpression - Left expression to test against the {@param rightExpressions}.
+	 * @param rightExpressions - Expressions to compare its values against {@param leftExpression}.
+	 */
+	in( leftExpression:Expression | SupportedNativeTypes, ...rightExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns true whether
+	 * the {@param leftExpression} is NOT equal to any of the {@param rightExpressions} values.
+	 *
+	 * See https://www.w3.org/TR/sparql11-query/#func-in
+	 * for more information.
+	 *
+	 * @param leftExpression - Left expression to test against the {@param rightExpressions}.
+	 * @param rightExpressions - Expressions to compare its values against {@param leftExpression}.
+	 */
+	notIn( leftExpression:Expression | SupportedNativeTypes, ...rightExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
 
-	add( expression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
-	subtract( expression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns the arithmetic sum
+	 * between the {@param leftExpression} and every {@param restExpressions}.
+	 *
+	 * See the table *SPARQL Binary Operators* on
+	 * https://www.w3.org/TR/sparql11-query/#OperatorMapping for more information.
+	 *
+	 * @param leftExpression - Left expression for the arithmetic sum
+	 * @param restExpressions - Expressions to construct an arithmetic sum with the previous expression.
+	 */
+	add( leftExpression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns the arithmetic difference
+	 * between the {@param leftExpression} and every {@param restExpressions}.
+	 *
+	 * See the table *SPARQL Binary Operators* on
+	 * https://www.w3.org/TR/sparql11-query/#OperatorMapping for more information.
+	 *
+	 * @param leftExpression - Left expression for the arithmetic difference
+	 * @param restExpressions - Expressions to construct an arithmetic difference with the previous expression.
+	 */
+	subtract( leftExpression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
 
-	multiply( expression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
-	divide( expression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns the arithmetic product
+	 * between the {@param leftExpression} and every {@param restExpressions}.
+	 *
+	 * See the table *SPARQL Binary Operators* on
+	 * https://www.w3.org/TR/sparql11-query/#OperatorMapping for more information.
+	 *
+	 * @param leftExpression - Left expression for the arithmetic product
+	 * @param restExpressions - Expressions to construct an arithmetic product with the previous expression.
+	 */
+	multiply( leftExpression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns the arithmetic quotient
+	 * between the {@param leftExpression} and every {@param restExpressions}.
+	 *
+	 * See the table *SPARQL Binary Operators* on
+	 * https://www.w3.org/TR/sparql11-query/#OperatorMapping for more information.
+	 *
+	 * @param leftExpression - Left expression for the arithmetic quotient
+	 * @param restExpressions - Expressions to construct an arithmetic quotient with the previous expression.
+	 */
+	divide( leftExpression:Expression | SupportedNativeTypes, ...restExpressions:(Expression | SupportedNativeTypes)[] ):Expression;
 
+	/**
+	 * Creates an operation {@link Expression} that returns true
+	 * if the effective boolean value of the {@param expression} is false.
+	 *
+	 * See the table *SPARQL Unary Operators* on
+	 * https://www.w3.org/TR/sparql11-query/#OperatorMapping for more information.
+	 *
+	 * @param expression - Expression to return the inverse effective boolean value.
+	 */
 	not( expression:Expression | SupportedNativeTypes ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns the numeric
+	 * {@param expression} with its sing unchanged.
+	 *
+	 * See the table *SPARQL Unary Operators* on
+	 * https://www.w3.org/TR/sparql11-query/#OperatorMapping for more information.
+	 *
+	 * @param expression - Numeric expression to return with the sign unchanged.
+	 */
 	plus( expression:Expression | SupportedNativeTypes ):Expression;
+	/**
+	 * Creates an operation {@link Expression} that returns the numeric
+	 * {@param expression} with its sing reversed.
+	 *
+	 * See the table *SPARQL Unary Operators* on
+	 * https://www.w3.org/TR/sparql11-query/#OperatorMapping for more information.
+	 *
+	 * @param expression - Numeric expression to return with the sign reversed.
+	 */
 	minus( expression:Expression | SupportedNativeTypes ):Expression;
 }
 
