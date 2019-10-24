635545166930de82a6041ad6e7476e09de025767
Iñaki Janeiro Olague <inaki_janeiro@live.com.mx>
2019-10-24 18:12:53 -0500

    Add description buttons to the templates and fix typos

--- tasks/docs.ts
+++ tasks/docs.ts
@@ -1,23 +1,48 @@
 import gulp from "gulp";
 import DocsEngine from "carbonldp-ts-docs-engine";
 import path from "path";
-import del = require('del');
+import del = require("del");
+import fs from "fs";
 
 const projectRootDir:string = path.resolve( __dirname, "../" );
 const sourceDir:string = path.resolve( projectRootDir, "src/" );
 const outputDir:string = path.resolve( projectRootDir, "docs/" );
+const descriptionTemplateRoute:string = path.join(outputDir, "/templates/documentation-description.njk");
+
+function getDescriptionTemplate():Promise<string> {
+
+	return new Promise((resolve, reject) => {
+		fs.readFile(descriptionTemplateRoute, "utf8",  async (err:Error, data:string) => {
+			if (err) {
+				console.log("Couldn't find file at", descriptionTemplateRoute );
+				return reject(err);
+			}
+			return resolve(data);
+		});
+	});
+}
 
 export const docsClean:gulp.TaskFunction = () =>
-	del( [ outputDir ] );
+	del( [
+		`${outputDir}/**`, `!${outputDir}`,
+		`!${outputDir}/templates`, `!${outputDir}/templates/**`,
+	] );
 docsClean.displayName = "docs:clean";
 
-export const generateDocumentation:( env:"development" | "production" ) => gulp.TaskFunction = env => {
-	const fn:gulp.TaskFunction = () => {
+export const generateDocumentation:( env:"development" | "production", descriptionTemplateRoute:string ) => gulp.TaskFunction = env => {
+	const fn:gulp.TaskFunction = async () => {
+
+		let descriptionTemplate:string = await getDescriptionTemplate();
+
 		const options:DocsEngine.Options = {
 			src: sourceDir,
 			out: outputDir,
 			mode: env,
 			logLevel: "info",
+			descriptionTemplate: descriptionTemplate,
+			npmName: "sparqler",
+			name: "SPARQLER",
+			mainClass: "SPARQLER",
 		};
 		return DocsEngine.generate( options );
 	};
@@ -27,15 +52,15 @@ export const generateDocumentation:( env:"development" | "production" ) => gulp.
 
 export const docsBuildDev:gulp.TaskFunction = gulp.series(
 	docsClean,
-	generateDocumentation("development")
+	generateDocumentation("development", descriptionTemplateRoute)
 );
 docsBuildDev.displayName = "docs:build|dev";
 
 export const docsBuildProd:gulp.TaskFunction = gulp.series(
 	docsClean,
-	generateDocumentation("production")
+	generateDocumentation("production", descriptionTemplateRoute)
 );
-docsBuildDev.displayName = "docs:build|prod";
+docsBuildProd.displayName = "docs:build|prod";
 
 
 
