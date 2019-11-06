b9674fb88bfa7813e8b3c34372bd6bb9a3af5332
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2019-11-06 17:20:36 -0600

    Add tests of filter with function expressions

--- src/tokens/FilterToken.spec.ts
+++ src/tokens/FilterToken.spec.ts
@@ -1,5 +1,7 @@
 import { BracketedExpressionToken } from "./BracketedExpressionToken";
+import { ExpressionListToken } from "./ExpressionListToken";
 import { FilterToken } from "./FilterToken";
+import { FunctionToken } from "./FunctionToken";
 import { IRIRefToken } from "./IRIRefToken";
 import { UnaryOperationToken } from "./UnaryOperationToken";
 import { VariableToken } from "./VariableToken";
@@ -41,14 +43,25 @@ describe( "FilterToken", ():void => {
 			expect( FilterToken.prototype.toString ).toEqual( jasmine.any( Function ) );
 		} );
 
-		it( "should compact print the SPARQL FILTER statement", ():void => {
+		it( "should compact print the SPARQL FILTER statement with bracketed expression", ():void => {
 			const token:FilterToken = new FilterToken( new BracketedExpressionToken( new UnaryOperationToken( "!", new VariableToken( "foo" ) ) ) );
 			expect( token.toString() ).toBe( "FILTER(!?foo)" );
 		} );
 
-		it( "should pretty print the SPARQL FILTER statement", ():void => {
+		it( "should pretty print the SPARQL FILTER statement with bracketed expression", ():void => {
 			const token:FilterToken = new FilterToken( new BracketedExpressionToken( new UnaryOperationToken( "!", new VariableToken( "foo" ) ) ) );
 			expect( token.toString( 0 ) ).toBe( "FILTER( ! ?foo )" );
+		} );
+
+
+		it( "should compact print the SPARQL FILTER statement with function expression", ():void => {
+			const token:FilterToken = new FilterToken( new FunctionToken( "BOUND", new ExpressionListToken( [ new VariableToken( "foo" ) ] ) ) );
+			expect( token.toString() ).toBe( "FILTER BOUND(?foo)" );
+		} );
+
+		it( "should pretty print the SPARQL FILTER statement with function expression", ():void => {
+			const token:FilterToken = new FilterToken( new FunctionToken( "BOUND", new ExpressionListToken( [ new VariableToken( "foo" ) ] ) ) );
+			expect( token.toString( 0 ) ).toBe( "FILTER BOUND( ?foo )" );
 		} );
 
 	} );
