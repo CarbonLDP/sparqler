3294b5a74ad7141149574ee7997f3bf60c81d558
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2019-11-06 18:39:56 -0600

    Correct PatternBuilder.minus behavior

--- src/patterns/PatternBuilder.ts
+++ src/patterns/PatternBuilder.ts
@@ -4,9 +4,8 @@ import { Factory } from "../core/factories/Factory";
 import { SubSelectPattern } from "./clausePatterns/SubSelectPattern";
 import { Expression } from "./expressions/Expression";
 import { ExpressionsBuilder } from "./expressions/ExpressionsBuilder";
-import { getUnaryOperationFn } from "./expressions/fns/operationFn";
 import { MinusPattern } from "./notTriplePatterns/MinusPattern";
-import { getMinusFn, NotTriplePatternsBuilder } from "./notTriplePatterns/NotTriplePatternsBuilder";
+import { NotTriplePatternsBuilder } from "./notTriplePatterns/NotTriplePatternsBuilder";
 import { OrderBuilder } from "./orders/OrderBuilder";
 import { PathsBuilder } from "./paths/PathsBuilder";
 import { Pattern } from "./Pattern";
@@ -64,26 +63,31 @@ export const PatternBuilder:{
 	createFrom<C extends Container<undefined>, O extends object>( container:C, object:O ):O & PatternBuilder;
 } = {
 	createFrom<C extends Container<undefined>, O extends object>( container:C, object:O ):O & PatternBuilder {
-		return Factory.createFrom(
+		const patternBuilder = Factory.createFrom(
 			TriplePatternsBuilder.createFrom,
-			NotTriplePatternsBuilder.createFrom,
+			NotTriplePatternsBuilder.createFrom
+		)( container, object );
+
+		const _patternSupper = Object.assign( {}, patternBuilder );
+
+		const fullBuilder = Factory.createFrom(
 			SubSelectPattern.createFrom,
 			PathsBuilder.createFrom,
 			ExpressionsBuilder.createFrom,
 			OrderBuilder.createFrom,
-		)( container, Object.assign( object, {
-			minus: ( expressionOrPattens:Expression | SupportedNativeTypes | Pattern | Pattern[] ) => {
-				if(
-					typeof expressionOrPattens === "object" &&
-					(
-						Array.isArray( expressionOrPattens ) ||
-						"getPattern" in expressionOrPattens
-					)
-				)
-					return getMinusFn( container )( expressionOrPattens );
+		)( container, patternBuilder );
 
-				return getUnaryOperationFn( Expression.createFrom, container, "-" )( expressionOrPattens );
+		const _expressionSupper = Object.assign( {}, fullBuilder );
+
+		return Object.assign( fullBuilder, {
+			minus: ( expressionOrPattens:Expression | SupportedNativeTypes | Pattern | Pattern[] ) => {
+				if( typeof expressionOrPattens === "object" && (
+					Array.isArray( expressionOrPattens ) ||
+					"getPattern" in expressionOrPattens
+				) ) return _patternSupper.minus( expressionOrPattens );
+
+				return _expressionSupper.minus( expressionOrPattens );
 			},
-		} ) );
+		} );
 	},
 };
